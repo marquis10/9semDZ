422d016905351853f1bef516e47c77effdbf6e1c
Alexandr Kuzmin <kuzmin_89@mail.ru>
2023-01-14 12:30:32 +0500

    64z

---
+++ 64z/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using static System.Console;
+
+string PrintNumbers(int a, int b)
+{
+    if (b == a)
+        return b.ToString();
+    return (b + " " + PrintNumbers(a, b - 1));
+}
+
+Clear();
+Write("Введите число M: ");
+int m = int.Parse(ReadLine());
+Write("Введите число N: ");
+int n = int.Parse(ReadLine());
+WriteLine(PrintNumbers(m, n));
