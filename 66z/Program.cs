3f6edc3cec00f706c4e3d2e55c4b0f587efd3f0e
Alexandr Kuzmin <kuzmin_89@mail.ru>
2023-01-14 12:53:42 +0500

    66z

--- 66z/Program.cs
+++ 66z/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using static System.Console;
+
+int PrintSum(int a, int b)
+{
+    //int sum = a;
+    if (b == a)
+        return a;
+    int sum = b + PrintSum(a, b - 1);
+    return (sum);
+}
+
+Clear();
+Write("Введите значение M: ");
+int m = int.Parse(ReadLine());
+Write("Введите значение N: ");
+int n = int.Parse(ReadLine());
+WriteLine(PrintSum(m, n));
